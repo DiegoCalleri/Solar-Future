970e5f337c12d9fe4e0bb77e071e7c03ab2dc94e
denisamirov <102862671+DiegoCalleri@users.noreply.github.com>
2026-02-11 07:55:42 +0300

    feat(hardware): start port listener

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,14 @@ const cors = require('./middlewares/cors');
 const apiRouter = require('./routes/api');
 
 
-const connectToDatabase = require('./database/connect')
+const connectToDatabase = require('./database/connect');
+const tcpServer = require('./utils/tcp/server');
+
 const app = express();
 const port = process.env.PORT || 4000;
-connectToDatabase()
+const tcpPort = process.env.TCP_PORT || 7070;
+
+connectToDatabase();
 
 app.use(cors, 
         bodyParser.json(), 
@@ -19,6 +23,10 @@ app.get('/', (req, res) => {
   res.send('Home Route');
 });
 
-app.listen(port, () =>
-  console.log(`Server running on port ${port}, http://localhost:${port}`)
-);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}, http://localhost:${port}`);
+});
+
+tcpServer.listen(tcpPort, () => {
+  console.log(`TCP server (modem) listening on port ${tcpPort}`);
+});
